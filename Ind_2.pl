32d633650d8449fc5f7764930509fe02824c0561
DeadDDolphin <62338390+DeadDDolphin@users.noreply.github.com>
2020-04-14 10:35:11 +0300

    Update Ind_2.pl
    Пытался переделать.Психанул.Потом продолжу

--- Ind_2.pl
+++ Ind_2.pl
@@ -4,9 +4,9 @@ r_list(A,N,I,B):-I1 is I+1, read(X), append(B,[X],B1),r_list(A,N,I1,B1).
 w_list([]):-!.
 w_list([H|T]):-write(H),nl,w_list(T).
 
-prost:-read(A),pr_list(A,Z),w_list(Z).
-pr_list(A,[]):-bamba(A,2,[]).
-bamba(A,A,Z):-!.
-bamba(1,_,_):-fail.
-bamba(A,I,Z):-Y is A mod I, Y=0,append(Z,[I],Z1),A1 is A/I,bamba(A1,I,Z1),!.
-bamba(A,I,Z):-I1 is I+1,I<A,bamba(A,I1,Z).
+prost(Z):-read(A),pr_list(A,Z).
+pr_list(A,Z):-bamba(A,2,Z,[]).
+bamba(A,A,Z,Z):-!.
+bamba(1,_,Z,Z):-fail.
+bamba(A,I,Z,Z1):-Y is A mod I, Y=0,append(Z1,[I],K),A1 is A/I,bamba(A1,I,K,K),!.
+bamba(A,I,Z,Z):-I1 is I+1,I<A,bamba(A,I1,Z,Z).
