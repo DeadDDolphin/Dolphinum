9441210a944deeea509810eede02c04fabc2ce30
DeadDDolphin <62338390+DeadDDolphin@users.noreply.github.com>
2020-04-14 15:49:37 +0300

    Update Ind_2.pl
    Оно работает =)
    Это победа.

--- Ind_2.pl
+++ Ind_2.pl
@@ -4,9 +4,8 @@ r_list(A,N,I,B):-I1 is I+1, read(X), append(B,[X],B1),r_list(A,N,I1,B1).
 w_list([]):-!.
 w_list([H|T]):-write(H),nl,w_list(T).
 
-prost(Z):-read(A),pr_list(A,Z).
-pr_list(A,Z):-bamba(A,2,Z,[]).
-bamba(A,A,Z,Z):-!.
-bamba(1,_,Z,Z):-fail.
-bamba(A,I,Z,Z1):-Y is A mod I, Y=0,append(Z1,[I],K),A1 is A/I,bamba(A1,I,K,K),!.
-bamba(A,I,Z,Z):-I1 is I+1,I<A,bamba(A,I1,Z,Z).
+prost(X):-read(A),pr_list(A,X).
+pr_list(A,X):-bamba(A,2,[],X).
+bamba(1,_,X,X):-!.
+bamba(A,I,Z,X):-Y is A mod I, Y=0,append(Z,[I],Z2),A1 is A/I,bamba(A1,I,Z2,X),!.
+bamba(A,I,Z,X):-I1 is I+1,bamba(A,I1,Z,X).
