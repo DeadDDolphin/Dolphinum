82cd9ed4cec3cdf3373457a79c9dd5ee3a0124d6
DeadDDolphin <62338390+DeadDDolphin@users.noreply.github.com>
2020-05-13 15:04:32 +0300

    Update Ind_1.pl

--- Ind_1.pl
+++ Ind_1.pl
@@ -1,21 +1,18 @@
+pr(1):-!,fail.
 pr(2):-!.
 pr(X):-pr1(2,X).
 pr1(X,X):-!.
 pr1(I,X):-Y is X mod I,not(Y=0),I1 is I+1,pr1(I1,X).
-getR(X):-lalala(0,X).
-lalala(K,X):-X1 is
 
-chisla(S):-chis(0,S,999999),write(S).
-chis(S,S,X):-usech(X),S1 is S+X,X1 is X-1,chis(S1,S,X1).
-usechL(2):-!.
-usechL(3):-!.
-usechL(5):-!.
-usechL(7):-!.
-usechL(X):-pr(X),X1 is X/10, usechL(X1).
-usechR(2):-!.
-usechR(3):-!.
-usechR(5):-!.
-usechR(7):-!.
-usechR(X):-pr(X),X1
+interestingR(0):-!.
+interestingR(X):-pr(X),Y is X div 10, interestingR(Y).
+
+interestingL(X,K):-K>X*10,!.
+interestingL(X,K):- Y is X mod K, pr(Y),K1 is K*10, interestingL(X,K1).
+
+summa(1000000,Sum,Sum):-!.
+summa(N,Sum,S):-interestingL(N,10),interestingR(N),write(N),nl,S1 is S +N,N1 is N+1,summa(N1,Sum,S1).
+summa(N,Sum,S):-N1 is N+1,summa(N1,Sum,S).
+
 
 
