1dac8b0bccda5c9dc31d9caf59af990c0d213388
DeadDDolphin <62338390+DeadDDolphin@users.noreply.github.com>
2020-06-03 22:57:54 +0300

    В теории должно бы работать а на деле...
    ПАМАГИТИ

---
+++ ConsoleApp1/ConsoleApp1/Program.fs
@@ -0,0 +1,18 @@
+﻿let fiveDeg x = x*x*x*x*x
+
+let rec sumDeg f x s =
+    if x<1 then s
+    else (sumDeg f (x/10) (s+(f x%10)))
+ 
+let summa f g =
+    let rec sumChisel f g x s =
+        if x>50000 then s
+        else if (f g x 0) = x then (sumChisel f g (x+1) (s+x))
+            else (sumChisel f g (x+1) s)
+    sumChisel f g 2 0
+
+[<EntryPoint>]
+let main argv =
+    let sum = summa sumDeg fiveDeg
+    System.Console.WriteLine("Сумма: "+ System.Convert.ToString(sum));
+    0 // return an integer exit code
