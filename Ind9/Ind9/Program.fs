543361bbbcd8d9f0148b58a7533ec582b632f7c2
DeadDDolphin <62338390+DeadDDolphin@users.noreply.github.com>
2020-06-03 14:13:44 +0300

    9-я индвидуалка
    Без подключенной службы, но вроде должно работать

--- Ind9/Ind9/Program.fs
+++ Ind9/Ind9/Program.fs
@@ -1,5 +1,16 @@
-﻿
+﻿open System.Math;
+
+let rec sumDeg x s =
+    if x<0 then s
+    else (sumDeg (x/10) (s+Math.Pow(x%10,5)))
+
+let rec sumChisel f x s=
+    if x>50000 then s
+    else if (f x 0) = x then (sumChisel f (x+1) (s+x))
+         else (sumChisel f (x+1) s)
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let sum = (sumChisel sumDeg 2 0)
+    System.Console.WriteLine(sum);
     0 // return an integer exit code
