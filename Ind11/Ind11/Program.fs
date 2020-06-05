309aee85fd6c456e1a72e2dcc68bb532806ed08d
DeadDDolphin <62338390+DeadDDolphin@users.noreply.github.com>
2020-06-06 02:19:40 +0300

    11
    Вот пока то, что с 11 получилось

---
+++ Ind11/Ind11/Program.fs
@@ -0,0 +1,36 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open System.Windows.Forms
+open System. Drawing
+
+let myForm = new Form(Width= 1000, Height = 600, Text = "Индивидуальное задание, 8-й вариант", Menu = new MainMenu())
+
+let placeToInput = new TextBox(Width= 150)
+
+placeToInput.Location <- new Point( 200, 200)
+myForm.Controls.Add(placeToInput)
+
+let myCreatingList = new RichTextBox(Width = 150,Height = 400)
+myCreatingList.Location <- new Point(700,100)
+myForm.Controls.Add(myCreatingList)
+
+let myButton = new Button(Width= 140, Height = 60,Text = "Он сказал поехали, \nон взмахнул рукой!")
+myButton.Location <- new Point( 450, 350)
+myForm.Controls.Add(myButton)
+
+let x = 0
+let iter x = x+1
+let putNum = myCreatingList.AppendText(System.Convert.ToString(iter x))
+let putText = myCreatingList.AppendText(". "+placeToInput.Text+"\n")
+    
+
+let _ = myButton.Click.Add(fun evArgs -> putNum)
+let _ = myButton.Click.Add(fun evArgs ->putText)
+// Запускаем форму
+do Application.Run(myForm)
+
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // return an integer exit code
